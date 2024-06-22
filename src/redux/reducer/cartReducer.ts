ac8265765d876d31b0fa8b70274c1c9fe0765c42
asarkar6589 <sarkararnab266@gmail.com>
2024-06-22 16:35:32 +0530

    Cart logic

--- src/redux/reducer/cartReducer.ts
+++ src/redux/reducer/cartReducer.ts
@@ -1,81 +1,101 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
-import { ShippingAddress, cartItemsType, initialCartStateType } from "../../types/initialState-types";
+import {
+  ShippingAddress,
+  cartItemsType,
+  initialCartStateType,
+} from "../../types/initialState-types";
 
-const initialState:initialCartStateType = {
-    loading: false,
-    cartItems: [],
-    subtotal: 0,
-    shippingCharge: 0,
-    tax: 0,
-    discount: 0,
-    total: 0,
-    shippingAddress: {
-        address: "",
-        city: "",
-        country: "",
-        pinCode: 0
-    }
-}
+const initialState: initialCartStateType = {
+  loading: false,
+  cartItems: [],
+  subtotal: 0,
+  shippingCharge: 0,
+  tax: 0,
+  discount: 0,
+  total: 0,
+  shippingAddress: {
+    address: "",
+    city: "",
+    country: "",
+    pinCode: 0,
+  },
+};
 
 export const cartReducer = createSlice({
-    name: "cartReducer",
-    initialState,
-    reducers: {
-        addToCartFun: (state, action:PayloadAction<cartItemsType>) => {
-            // check -> If the item already exists or not. If the element is already present, then there is no need to add it again.
+  name: "cartReducer",
+  initialState,
+  reducers: {
+    addToCartFun: (state, action: PayloadAction<cartItemsType>) => {
+      // check -> If the item already exists or not. If the element is already present, then there is no need to add it again.
 
-            state.loading = true;
+      state.loading = true;
 
-            const index = state.cartItems.findIndex(i => i.id === action.payload.id);
+      const index = state.cartItems.findIndex(
+        (i) => i.id === action.payload.id
+      );
 
-            if (index === -1) {
-                state.cartItems.push(action.payload);
-            }
-            else {
-                state.cartItems[index] = action.payload;
-            }
+      if (index === -1) {
+        state.cartItems.push(action.payload);
+      } else {
+        state.cartItems[index] = action.payload;
+      }
 
-            state.loading = false;
-        },
-        removeFromCart: (state, action:PayloadAction<string>) => {
-            // here we will just take the id of the product and remove it from the cart array (filter)
+      state.loading = false;
+    },
+    removeFromCart: (state, action: PayloadAction<string>) => {
+      // here we will just take the id of the product and remove it from the cart array (filter)
 
-            state.loading = true;
-            state.cartItems = state.cartItems.filter(c => c.id != action.payload);
-            state.loading = false;
-        },
-        calculatePrice: (state) => {
-            state.loading = true;
-            
-            // subtotal
-            let total = 0
-            for (let i = 0; i < state.cartItems.length; i++) {
-                total += state.cartItems[i].price * state.cartItems[i].quantity;
-            }
-            state.subtotal = total;
+      state.loading = true;
+      state.cartItems = state.cartItems.filter((c) => c.id != action.payload);
+      state.loading = false;
+    },
+    calculatePrice: (state) => {
+      state.loading = true;
 
-            // shipping charges
-            if (state.subtotal > 500) {
-                state.shippingCharge = 0;
-            }
-            else {
-                state.shippingCharge = 50;
-            }
+      if (state.cartItems.length === 0) {
+        state.discount = 0;
+      } else {
+        // subtotal
+        let total = 0;
+        for (let i = 0; i < state.cartItems.length; i++) {
+          total += state.cartItems[i].price * state.cartItems[i].quantity;
+        }
+        state.subtotal = total;
 
-            // tax
-            state.tax = Math.round(state.subtotal * 0.18); // 18% of the total amount
+        // shipping charges
+        if (state.subtotal > 500) {
+          state.shippingCharge = 0;
+        } else {
+          state.shippingCharge = 50;
+        }
 
-            // discount -> We will calculate it on the time of billing
-            state.total = state.subtotal + state.shippingCharge + state.tax - state.discount;
-        },
-        calculateDiscount: (state, action:PayloadAction<number>) => {
-            state.discount = action.payload;
-        },
-        saveShippingInfo: (state, action:PayloadAction<ShippingAddress>) => {
-            state.shippingAddress = action.payload;
-        },
-        resetCart : () => initialState
-    }
+        // tax
+        state.tax = Math.round(state.subtotal * 0.18); // 18% of the total amount
+
+        // discount -> We will calculate it on the time of billing
+        state.total =
+          state.subtotal + state.shippingCharge + state.tax - state.discount;
+      }
+    },
+    calculateDiscount: (state, action: PayloadAction<number>) => {
+      if (state.cartItems.length === 0) {
+        state.discount = 0;
+      } else {
+        state.discount = action.payload;
+      }
+    },
+    saveShippingInfo: (state, action: PayloadAction<ShippingAddress>) => {
+      state.shippingAddress = action.payload;
+    },
+    resetCart: () => initialState,
+  },
 });
 
-export const {addToCartFun, removeFromCart, calculatePrice, calculateDiscount, resetCart, saveShippingInfo} = cartReducer.actions;
+export const {
+  addToCartFun,
+  removeFromCart,
+  calculatePrice,
+  calculateDiscount,
+  resetCart,
+  saveShippingInfo,
+} = cartReducer.actions;
